0d3f2b3fc16441df7ce18cabd1ccf29862518848
samybellil <samybellil15@gmail.com>
2025-10-03 15:56:13 +0200

    update bank_account

--- bank_account.py
+++ bank_account.py
@@ -1,5 +1,44 @@
-# ================= ROUTES FLASK ================= #
 
+import random
+from flask import Flask, render_template, request,redirect, url_for, session
+
+# Création de l'application Flask
+app = Flask(__name__)
+app.secret_key = "supersecret"  # nécessaire pour la session (login)
+
+# Classe Compte
+class Account:
+    def __init__(self, name, balance=2000):
+        self.name = name
+        self.account_number = random.randint(1000000000, 9999999999)
+        self.balance = balance
+        self.history = []
+
+    def deposit(self, amount):
+        self.balance += amount
+        self.history.append(f"Dépôt de {amount} € → Solde: {self.balance} €")
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            self.history.append(f"Échec retrait {amount} € (fonds insuffisants)")
+        else:
+            self.balance -= amount
+            self.history.append(f"Retrait de {amount} € → Solde: {self.balance} €")
+
+# Comptes de test
+accounts = {
+    "Ross": Account("Ross"),
+    "Rachel": Account("Rachel")
+}
+
+# On simule quelques transactions
+accounts["Ross"].deposit(500)
+accounts["Ross"].withdraw(1150)
+
+accounts["Rachel"].deposit(1500)
+accounts["Rachel"].withdraw(50)
+
+# ================= ROUTES FLASK ================= #
 # Page de login
 @app.route("/", methods=["GET", "POST"])
 def login():
